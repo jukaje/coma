f89b6ed2bbcd247e4698d478cb60ace59a036e08
jukaje <jud.jennert@campus.lmu.de>
2023-01-26 18:58:28 +0100

    files

---
+++ NEWS.md
@@ -0,0 +1,1 @@
+# coma 0.0.1 *initial version
---
+++ R/utils.R
@@ -0,0 +1,13 @@
+#' @title Incremental function
+#' @description This function increments a variable by a given value. That's it.
+#' @param t summand 1
+#' @param s summand 2
+#' 
+#' @examples 
+#' x <- 1
+#' x %+=% 3
+#' x
+#' #> [1] 4
+#' @export
+
+`%+=%` = function(t, s) eval.parent(substitute((t) <- (t) + (s)))
